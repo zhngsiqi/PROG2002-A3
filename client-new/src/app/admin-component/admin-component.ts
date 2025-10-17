4b3989e261031483a1d281652c0f85042a89aeda
guoxiuguoxiu <161835664+guoxiuguoxiu@users.noreply.github.com>
2025-10-17 17:09:36 +0800

    design admin navigation panel

--- client-new/src/app/admin-component/admin-component.ts
+++ client-new/src/app/admin-component/admin-component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import {Navigation} from '../navigation/navigation';
-import {RouterOutlet} from '@angular/router';
+import {RouterModule, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-admin-component',
-  imports: [Navigation, RouterOutlet],
+  imports: [Navigation, RouterOutlet, RouterModule],
   templateUrl: './admin-component.html',
   styleUrl: './admin-component.css'
 })
