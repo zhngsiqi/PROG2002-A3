ee190f05262de5ff4e45b0f37c6ed9d61018e0e9
zhngsiqi <s.zhang.42@student.scu.edu.au>
2025-10-17 16:38:24 +0800

    add weather feature

--- client-new/src/app/event-component/event-component.ts
+++ client-new/src/app/event-component/event-component.ts
@@ -16,13 +16,19 @@ export class EventComponent {
   event: any;
   err = ""
   registrations: any[] = [] // Array to save registration records
+  weather: any = null; // weather data
 
   constructor(private eventService: EventService, private route: ActivatedRoute, private registrationService: RegistrationService, private router: Router) {
     const id = this.route.snapshot.params['id'];
 
     // Get event details
-    this.eventService.fetchSingleEvent(id).subscribe(result => {
+    this.eventService.fetchSingleEvent(id).subscribe((result: any) => {
       this.event = result
+
+      // fetch weather information
+      if (result.latitude && result.longitude) {
+        this.fetchWeather(result.latitude, result.longitude);
+      }
     }, err => {
       this.err = err?.error?.message || err?.error?.error?.message
     })
@@ -37,5 +43,49 @@ export class EventComponent {
 
   toRegister() {
     this.router.navigateByUrl('/register/' + this.event.event_id)
+  }
+
+  fetchWeather(lat: number, lng: number) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        // get daily
+        const today = data.daily;
+
+        // get max and min temperature
+        this.weather = {
+          max: today.temperature_2m_max[0],
+          min: today.temperature_2m_min[0],
+          code: today.weather_code[0]
+        };
+      })
+      .catch(err => {
+        console.error('Weather API error:', err);
+      });
+  }
+
+  // get weather descirption by code
+  getWeatherDescription(code: number): string {
+    const mapping: any = {
+      0: 'Clear sky',
+      1: 'Mainly clear',
+      2: 'Partly cloudy',
+      3: 'Overcast',
+      45: 'Fog',
+      48: 'Depositing rime fog',
+      51: 'Light drizzle',
+      53: 'Moderate drizzle',
+      55: 'Dense drizzle',
+      61: 'Light rain',
+      63: 'Moderate rain',
+      65: 'Heavy rain',
+      80: 'Light rain showers',
+      81: 'Moderate rain showers',
+      82: 'Violent rain showers',
+      95: 'Thunderstorm',
+    };
+    return mapping[code] || 'Unknown';
   }
 }
